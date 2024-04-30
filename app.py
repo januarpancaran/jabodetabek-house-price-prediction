68df954248f5365e67ba334f0c4dad6bebc19a99
januarpancaran <januar352@gmail.com>
2024-04-30 21:00:05 +0700

    Menambahkan pages html dan styling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
+from train.train import df
 
 app = Flask(__name__)
 
@@ -8,23 +9,26 @@ model = pickle.load(open('train/model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('pages/hero.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
-    features = [np.array(int_features)]
-    prediction = model.predict(features)
-    scaled_prediction = prediction[0]
+    if request.method == 'POST':
+        int_features = [float(x) for x in request.form.values()]
+        features = [np.array(int_features)]
+        prediction = model.predict(features)
+        scaled_prediction = prediction[0]
 
-    if scaled_prediction >= 1e12:
-        output = "Rp{:.2f} Triliun".format(scaled_prediction / 1e12)
-    elif scaled_prediction >= 1e9:
-        output = "Rp{:.2f} Miliar".format(scaled_prediction / 1e9)
+        if scaled_prediction >= 1e12:
+            output = "Rp{:.2f} Triliun".format(scaled_prediction / 1e12)
+        elif scaled_prediction >= 1e9:
+            output = "Rp{:.2f} Miliar".format(scaled_prediction / 1e9)
+        else:
+            output = "Rp{:.2f} Juta".format(scaled_prediction / 1e6)
+
+        return render_template('pages/predict.html', prediction_text=output)
     else:
-        output = "Rp{:.2f} Juta".format(scaled_prediction / 1e6)
-
-    return render_template('index.html', prediction_text=output)
+        return render_template('pages/predict.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
